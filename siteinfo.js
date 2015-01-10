bdbf3edd1dc65a5b721863a902c4b26f84155771
Eric Willis <eric@ecwillis.com>
2015-01-10 15:27:27 -0800

    Updating style changes per new jscs

--- siteinfo.js
+++ siteinfo.js
@@ -1,80 +1,70 @@
 
-var $url = require('url'),
-$request = require('request'),
-$ch = require('cheerio');
+var $url = require('url');
+var $request = require('request');
+var $ch = require('cheerio');
 
-function SiteInfo(url, err, cb){
+var SiteInfo = function(url, err, cb) {
 
-  var _this = {};
+  var self = {};
 
-  function init(url, err, cb)
-  {
-    _this.url_object = {};
+  var init = function(url, err, cb) {
+    self.urlObject = {};
 
-    if(url !== '' && url !== null)
-    {
-      _this.url_object = $url.parse(url);
+    if (url !== '' && url !== null) {
+      self.urlObject = $url.parse(url);
     }
-    
-    _this.url = url;
-    _this.err_cb = err;
-    _this.cb = cb;
+    self.url = url;
+    self.errCb = err;
+    self.cb = cb;
 
     getSite(url);
 
-    return _this;
+    return self;
   }
 
-  function absPath(path)
-  {
-    url_str = _this.url_object.protocol + "://" + _this.url_object.hostname;
-    if( _this.url_object.pathname != '/')
-    {
-      url_str +=  _this.url_object.pathname;
+  var absPath = function(path) {
+    urlStr = self.urlObject.protocol + '://' + self.urlObject.hostname;
+    if ( self.urlObject.pathname != '/') {
+      urlStr +=  self.urlObject.pathname;
     }
 
-    return url_str + path.replace( url_str + "" );
+    return urlStr + path.replace( urlStr );
   }
 
-  function findData(documentBody)
-  {
+  var findData = function(documentBody) {
     // Check to see if there is data
 
-    if( documentBody === '')
-    {
-      _this.err_cb({ 'message' : 'The document is empty'})
+    if ( documentBody === '') {
+      self.errCb({ message: 'The document is empty' })
     }
 
     $ = $ch.load( documentBody );
 
     var data = {
-      'pageTitle'          : null,
-      'description'         : null,
-      'descriptionSource'  : null,
-      'presumedFavicon'    : absPath('/favicon.ico'),
-      'favicon'             : null,
-      'mainImage'          : null,
-      'images'              : []
+      pageTitle: null,
+      description: null,
+      descriptionSource: null,
+      presumedFavicon: absPath('/favicon.ico'),
+      favicon: null,
+      mainImage: null,
+      images: []
     };
 
     // Find the page title
-    if($('title').text() != '')
-    {
+    if ($('title').text() != '') {
       data.pageTitle = $('title').text();
     }
 
     // First, og:description
-    if($('meta[property="og:description"]').length > 0)
-    {
+    if ($('meta[property="og:description"]').length > 0) {
       data.description = $('meta[property="og:description"]').attr('content');
-      data.description_source = 'og:description';
-    } else if($('meta[property="description"]').length > 0) {
+      data.descriptionSource = 'og:description';
+    } else if ($('meta[property="description"]').length > 0) {
       data.description = $('meta[property="description"]').attr('content');
       data.descriptionSource = 'description';
     }
 
-    if( $('link[rel="shortcut icon"]').length > 0)
-    {
+    if ( $('link[rel="shortcut icon"]').length > 0) {
       data.favicon = absPath($('link[rel="shortcut icon"]').attr('href'));
     }
 
@@ -82,43 +72,38 @@ function SiteInfo(url, err, cb){
     var images = $('img');
 
     // Next, og:image
-    if($('meta[property="og:image"]').length > 0)
-    {
+    if ($('meta[property="og:image"]').length > 0) {
       data.mainImage = $('meta[property="og:image"]').attr('content');
     } else {
       // Now where are we going to find an image?
       var firstImage = images.get(0);
-      if( firstImage !== undefined )
-      {
+      if (firstImage !== undefined ) {
         data.mainImage = absPath( $(firstImage).attr('src') );
       }
     }
 
-    images.each(function(i){
-      if($(this).attr('src') !== '')
-      {
+    images.each(function(i) {
+      if ($(this).attr('src') !== '') {
         data.images.push(absPath( $(this).attr('src') ));
       }
     });
 
-    _this.cb( data );
+    self.cb( data );
 
   }
 
-  function getSite(url)
-  {
-    if(url === '')
-    {
-      _this.err_cb({'message' : 'URL Can not be blank.'});
+  var getSite = function(url) {
+    if (url === '') {
+      self.errCb({ message: 'URL Can not be blank.' } );
       return;
     }
 
-    $request(_this.url, function(error, response, body){
+    $request(self.url, function(error, response, body) {
       if (!error && response.statusCode == 200) {
         findData(body);
       } else {
         // Send the error callback
-        _this.err_cb( { 'message' : "There was an error retrieving the url" } );
+        self.errCb( { message: 'There was an error retrieving the url' } );
       }
     });
   }
@@ -127,7 +112,6 @@ function SiteInfo(url, err, cb){
 
 }
 
-module.exports = function(url, err, cb)
-{
+module.exports = function(url, err, cb) {
   return SiteInfo(url, err, cb);
 }
--- test/helpers/test_server.js
+++ test/helpers/test_server.js
@@ -1,13 +1,12 @@
 
 var http = require('http');
 
-var server = module.exports = http.createServer(function (req, res) {
-  if(req.url === '/')
-  {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+var server = module.exports = http.createServer( function(req, res) {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<html><head><title>This is a test.</title></head><body><img src="/thugsaretripping.jpg" /></body></html>');
   } else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.writeHead(404, { 'Content-Type': 'text/html' });
     res.end('<html><head><title>This is a test.</title></head><body><h1>ERRR!</h1></body></html>');
   }
 
